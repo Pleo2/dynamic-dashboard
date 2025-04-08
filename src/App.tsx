98562645773b3c3113d83b7bca2c04ef11873e7c
pleo2 <leooel23m@gmail.com>
2025-04-07 21:38:59 -0400

    feat: enhance App component with background image and updated greeting style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
     return (
         <>
-            <div className="grid place-items-center h-screen w-screen text-6xl">
-                Hello world!
+            <div className="grid place-items-center h-screen w-screen text-6xl bg-[url('./assets/wallpaper.png')] bg-cover bg-center bg-blue-800 opacity-95">
+                <h1 className="text-white font-bold text-8xl">
+                    Hello World!
+                </h1>
             </div>
         </>
     );
